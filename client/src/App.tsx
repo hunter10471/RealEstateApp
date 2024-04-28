7763f19602621d8ea2de4caee00473d1be2db25c
hunter10471 <rafayzia10@gmail.com>
2024-04-28 17:13:56 +0500

    Setup react-router-dom, added login,single pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,36 @@
-import Navbar from "./components/Medium/Navbar/navbar";
-import "./layout.scss";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./pages/Layout/Layout";
 import Home from "./pages/Home/Home";
+import List from "./pages/List/List";
+import Login from "./pages/Login/Login";
+import Single from "./pages/Single/Single";
 
 function App() {
-	return (
-		<div className="layout">
-			<div className="navbar">
-				<Navbar />
-			</div>
-			<div className="content">
-				<Home />
-			</div>
-		</div>
-	);
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <Layout />,
+			children: [
+				{
+					path: "/",
+					element: <Home />,
+				},
+				{
+					path: "/list",
+					element: <List />,
+				},
+				{
+					path: "/login",
+					element: <Login />,
+				},
+				{
+					path: "/:id",
+					element: <Single />,
+				},
+			],
+		},
+	]);
+	return <RouterProvider router={router} />;
 }
 
 export default App;
